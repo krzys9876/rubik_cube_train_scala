c9f6962f133b2488574726cc6812f768d05d0860
Krzysztof Ruta <krzysztof.ruta@play.pl>
2026-01-28 17:53:39 +0100

    start reorganizing moves - represent move as edge-to-edge replacement

--- src/main/scala/org/kr/cube/Cube2x2.scala
+++ src/main/scala/org/kr/cube/Cube2x2.scala
@@ -1,5 +1,7 @@
 package org.kr.cube
 
+import org.kr.cube.Cube2x2.s
+
 case class Cube2x2(state: String):
   //assert(state.length == 2*2*6)
   lazy val isSolved: Boolean = state == Cube2x2.SOLVED_STATE
@@ -21,15 +23,36 @@ object Cube2x2:
 sealed abstract class Move2x2(val symbol: String):
   def applyToState(state: String): String = ???
 
+  def edgeToEdge(origState: String, state: String,
+                 faceFrom: Face2x2, indexFrom1: Int, indexFrom2: Int,
+                 faceTo: Face2x2, indexTo1: Int, indexTo2: Int): String =
+    replaceOne(origState,
+      replaceOne(origState, state, faceFrom, indexFrom1, faceTo, indexTo1),
+      faceFrom, indexFrom2, faceTo, indexTo2)
+
+  private def replaceOne(origState: String, state: String,
+                         faceFrom: Face2x2, indexFrom: Int, faceTo: Face2x2, indexTo: Int): String =
+    state.substring(0, faceTo.index + indexTo) + Cube2x2.s(origState, faceFrom, indexFrom) +
+      state.substring(faceTo.index + indexTo + 1)
+
+
 object Moves2x2:
   case object F extends Move2x2("F"):
     override def applyToState(state: String): String =
-      Cube2x2.s(state, Face2x2.F, 2) + Cube2x2.s(state, Face2x2.F, 0) + Cube2x2.s(state, Face2x2.F, 3) + Cube2x2.s(state, Face2x2.F, 1) +
-        Cube2x2.s(state, Face2x2.L, 0) + Cube2x2.s(state, Face2x2.D, 0) + Cube2x2.s(state, Face2x2.L, 2) + Cube2x2.s(state, Face2x2.D, 1) +
-        Cube2x2.f(state, Face2x2.B) +
-        Cube2x2.s(state, Face2x2.U, 2) + Cube2x2.s(state, Face2x2.R, 1) + Cube2x2.s(state, Face2x2.U, 3) + Cube2x2.s(state, Face2x2.R, 3) +
-        Cube2x2.s(state, Face2x2.U, 0) + Cube2x2.s(state, Face2x2.U, 1) + Cube2x2.s(state, Face2x2.L, 3) + Cube2x2.s(state, Face2x2.L, 1) +
-        Cube2x2.s(state, Face2x2.R, 2) + Cube2x2.s(state, Face2x2.R, 0) + Cube2x2.s(state, Face2x2.D, 2) + Cube2x2.s(state, Face2x2.D, 3)
+      val stage1 = edgeToEdge(state, state, Face2x2.F, 2, 0, Face2x2.F, 0, 1)
+      val stage2 = edgeToEdge(state, stage1, Face2x2.F, 3, 1, Face2x2.F, 2, 3)
+      val stage3 = edgeToEdge(state, stage2, Face2x2.D, 0, 1, Face2x2.L, 1, 3)
+      val stage4 = edgeToEdge(state, stage3, Face2x2.L, 1, 3, Face2x2.U, 2, 3)
+      val stage5 = edgeToEdge(state, stage4, Face2x2.U, 2, 3, Face2x2.R, 0, 2)
+      val stage6 = edgeToEdge(state, stage5, Face2x2.R, 2, 0, Face2x2.D, 0, 1)
+      stage6
+
+  /*Cube2x2.s(state, Face2x2.F, 2) + Cube2x2.s(state, Face2x2.F, 0) + Cube2x2.s(state, Face2x2.F, 3) + Cube2x2.s(state, Face2x2.F, 1) +
+    Cube2x2.s(state, Face2x2.L, 0) + Cube2x2.s(state, Face2x2.D, 0) + Cube2x2.s(state, Face2x2.L, 2) + Cube2x2.s(state, Face2x2.D, 1) +
+    Cube2x2.f(state, Face2x2.B) +
+    Cube2x2.s(state, Face2x2.U, 2) + Cube2x2.s(state, Face2x2.R, 1) + Cube2x2.s(state, Face2x2.U, 3) + Cube2x2.s(state, Face2x2.R, 3) +
+    Cube2x2.s(state, Face2x2.U, 0) + Cube2x2.s(state, Face2x2.U, 1) + Cube2x2.s(state, Face2x2.L, 3) + Cube2x2.s(state, Face2x2.L, 1) +
+    Cube2x2.s(state, Face2x2.R, 2) + Cube2x2.s(state, Face2x2.R, 0) + Cube2x2.s(state, Face2x2.D, 2) + Cube2x2.s(state, Face2x2.D, 3)*/
 
   case object F1 extends Move2x2("F'"):
     override def applyToState(state: String): String =
@@ -60,5 +83,3 @@ object Face2x2:
   case object R extends Face2x2("R", 12)
   case object U extends Face2x2("U", 16)
   case object D extends Face2x2("D", 20)
-
-case class TileCoords(face: Face2x2, index: Int)
