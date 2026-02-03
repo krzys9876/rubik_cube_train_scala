b313fe087d7f8f38c3a00fbe7a7f13e72247796d
Krzysztof Ruta <krzysztof.ruta@play.pl>
2026-02-03 22:29:53 +0100

    play with solving

--- src/main/scala/org/kr/cube/main.scala
+++ src/main/scala/org/kr/cube/main.scala
@@ -24,21 +24,22 @@ def main(): Unit = {
     "q-values-2x2-upper-layer-10000000-20260203_151354.txt"
   )*/
   //debugUpperLayer("q-values-2x2-upper-layer-10000000-20260203_094831.txt")
-  solveOneFromScramble(
+  /*solveOneFromScramble(
     "q-values-2x2-white-layer-3000000-20260203_141521.txt",
     "q-values-2x2-yellow-layer-1000000-20260203_143242.txt",
     "q-values-2x2-upper-layer-10000000-20260203_151354.txt",
     "B B L' R B R B' U F' R U D R' B' D"
-  )
+  )*/
 
-  /*solveOneFromState(
+  solveOneFromState(
     "q-values-2x2-white-layer-3000000-20260203_141521.txt",
     "q-values-2x2-yellow-layer-1000000-20260203_143242.txt",
     "q-values-2x2-upper-layer-10000000-20260203_151354.txt",
     //"BFULLRFBFUBDLUDRFRUDLBRD"
     //"UDFLULDUDBRBLFFURRBBLDRF"
-    "UFRBLRRFDDBDULRLBFFLDUBU"
-  )*/
+    //"UFRBLRRFDDBDULRLBFFLDUBU"
+    "LDRFFBBUDLLUBBRDULURFDRF"
+  )
 
   /*solveRandomOne(
     "q-values-2x2-white-layer-3000000-20260203_141521.txt",
